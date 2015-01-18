deb13b6a7dd384b6f5d4a832f28bc188ece83f20
Janne Liuhtonen <janne.liuhtonen@iki.fi>
2015-01-18 09:45:43 +0200

    Reorder types

--- lastfm-dump.hs
+++ lastfm-dump.hs
@@ -7,16 +7,30 @@ import Data.Aeson
 import Data.Text
 import GHC.Generics
 import qualified Data.ByteString.Lazy.Char8 as C8 (pack, unpack)
-import Data.ByteString as BString
+import qualified Data.ByteString as BString
 
-data Track = Track {
-    name :: !Text           
+data RecentTracks = RecentTracks {
+    recenttracks :: TrackContainer
 } deriving (Show, Generic)
+
+instance FromJSON RecentTracks
 
 data TrackContainer = TrackContainer {
     track :: [Track],                    
     attr :: Attributes    
 } deriving (Show, Generic)
+
+instance FromJSON TrackContainer where
+        parseJSON (Object v) = TrackContainer <$>
+                            v .: "track" <*>
+                            v .: "@attr"
+        parseJSON _ = mzero
+
+data Track = Track {
+    name :: !Text
+} deriving (Show, Generic)
+
+instance FromJSON Track
 
 data Attributes = Attributes {
     user :: !Text,
@@ -26,19 +40,7 @@ data Attributes = Attributes {
     total :: !Text    
 } deriving (Show, Generic)
 
-data RecentTracks = RecentTracks {
-    recenttracks :: TrackContainer
-} deriving (Show, Generic)
-
-instance FromJSON RecentTracks
-instance FromJSON TrackContainer where
-        parseJSON (Object v) = TrackContainer <$>
-                            v .: "track" <*>
-                            v .: "@attr"
-        parseJSON _ = mzero
-
 instance FromJSON Attributes
-instance FromJSON Track
 
 fetchTracks :: IO (Maybe RecentTracks)
 fetchTracks = fmap decode $ simpleHttp "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=badg&api_key=cc5a08a82ef3d31fef33894f0fbd54cc&format=json"
