8d58086a2ca62ea5c0472ee5af0e86aef8b293e5
Janne Liuhtonen <janne.liuhtonen@iki.fi>
2015-01-19 21:36:07 +0200

    build url params

--- src/Main.hs
+++ src/Main.hs
@@ -10,6 +10,7 @@ import qualified Data.Aeson.Types as AesonTypes
 import qualified Data.Text as Text
 import Data.Time
 import Data.Maybe
+import Data.List (intercalate)
 import Data.Time.Clock.POSIX
 import qualified Data.ByteString.Lazy.Char8 as C8 (pack, unpack, putStrLn, writeFile)
 import qualified LastFm
@@ -17,15 +18,34 @@ import qualified LastFm
 apiKey = "cc5a08a82ef3d31fef33894f0fbd54cc"
 apiCallDelay = 1000000 -- 1 sec in microseconds
 pageSize = 200
+baseUrl = "http://ws.audioscrobbler.com/2.0/"
 
-urlParam :: Show a => String -> Maybe a -> String
-urlParam pName mValue = maybe "" f mValue where
-    f = ((++) (pName ++ "=")) . show
+removeQuotes :: String -> String
+removeQuotes = filter $ not . ((==) '"')
+
+toPlainString :: Show a => a -> String
+toPlainString = removeQuotes . show
+
+urlParam :: Show a => String -> a -> String
+urlParam key value = key ++ "=" ++ (toPlainString value)
+
+optUrlParam :: Show a => String -> Maybe a -> String
+optUrlParam key mValue = maybe "" (urlParam key) mValue
+
+buildParams :: String -> Maybe Int -> String
+buildParams user page = "?" ++ intercalate "&" params  where
+        params = [methodP, userP, limitP, formatP, apiKeyP, pageP]
+        methodP = urlParam "method" "user.getrecenttracks"
+        userP = urlParam "user" user
+        limitP = urlParam "limit" pageSize
+        apiKeyP = urlParam "api_key" apiKey
+        formatP = urlParam "format" "json"
+        pageP = optUrlParam "page" page
 
 fetchTracks :: Maybe Int -> IO (Maybe LastFm.RecentTracksResponse)
 fetchTracks page = do
-                      let pageParam' = urlParam "page" page
-                      let url = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=badg&limit=200&format=json&api_key=" ++ apiKey ++ "&" ++ pageParam'
+                      let pageParam' = optUrlParam "page" page
+                      let url = baseUrl ++ buildParams "badg" page 
                       putStrLn $ "url" ++ url
                       httpResponse <- simpleHttp url
                       putStrLn (show httpResponse)
